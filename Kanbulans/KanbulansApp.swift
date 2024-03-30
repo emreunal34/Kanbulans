1db9b73ed6ea71fa326f0f8ac4f5364dc2c2bed1
Emre Ünal <emreunl064@gmail.com>
2024-03-30 13:04:13 +0300

    Initial Commit

---
+++ Kanbulans/KanbulansApp.swift
@@ -0,0 +1,17 @@
+//
+//  KanbulansApp.swift
+//  Kanbulans
+//
+//  Created by Caner Ünal on 30.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct KanbulansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
